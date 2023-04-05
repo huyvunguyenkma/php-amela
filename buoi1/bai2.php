271499fd881d2be02c5d15210193b15dc3e45f9a
huyvunguyenkma <110873587+huyvunguyenkma@users.noreply.github.com>
2023-04-05 11:55:23 +0700

    Update bai2.php

--- buoi1/bai2.php
+++ buoi1/bai2.php
@@ -7,12 +7,8 @@
     ];
     $project["name"] =  "Làm giàu";
     $var = ["Nguyễn Văn A"];
-    array_push($project["developer"], "Dev A");
-    array_push($project["developer"], "Dev B");
-    array_push($project["developer"], "Dev C");
-    array_push($project["tester"], "Test A");
-    array_push($project["tester"], "Test B");
-    array_push($project["tester"], "Test C");
+    $project["developer"] = ["Dev A", "Dev B", "Dev C"];
+    $project["tester"] = ["Test A", "Test B", "Test C"];
 
     array_push($project, $project["comtor"] = "Comtor A");
 
